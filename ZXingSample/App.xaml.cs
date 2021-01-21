09c1cfb8e11051786da4b0b7d5358eaec2ef9db4
jswoo <jswoo@nifco.co.kr>
2021-01-21 15:53:52 +0900

    add

--- ZXingSample/App.xaml.cs
+++ ZXingSample/App.xaml.cs
@@ -17,6 +17,7 @@ namespace ZXingSample
 			////LoginPage.Children.Add(new GenerateBarcodePage());
             //
 			//LoginPage = LoginPage;
+            
 
             MainPage = new NavigationPage(new LoginPage());
             
