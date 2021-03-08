a4f2517cfb9176c936c9e9ad7f293b25732c2dff
WOOJiSeung <123www1@naver.com>
2021-03-08 17:31:35 +0900

    dasd

--- ZXingSample/BluetoothScanner.xaml.cs
+++ ZXingSample/BluetoothScanner.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using Android.Bluetooth;
 using Java.Util;
 using Plugin.BLE;
+using Plugin.BLE.Abstractions;
+using Plugin.BLE.Abstractions.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
@@ -18,6 +21,11 @@ namespace ZXingSample
         static UUID MY_UUID_INSECURE = UUID.FromString("00001101-0000-1000-8000-00805F9B34FB");
         BluetoothAdapter btAdapter;
 
+        IBluetoothLE ble;
+        IAdapter adapter;
+        IDevice device;
+        CancellationTokenSource _cancellationTokenSource;
+
         public BluetoothScanner ()
 		{
 			InitializeComponent ();
@@ -27,20 +35,45 @@ namespace ZXingSample
 
         private void _ConnectionScanner()
         {
+            
+
+            ConnectToMyDevice();
+
+            //var pairedDevices = adapter.GetSystemConnectedOrPairedDevices();
+            //adapter.StartScanningForDevicesAsync();
+            //var pairedDevices = adapter.ConnectedDevices;
+            //if (pairedDevices.Count > 0)
+            //{
+            //    foreach (var device in pairedDevices)
+            //    {
+            //        //pairedDevicesArrayAdapter.Add(device.Name + "\n" + device.Address);
+            //    }
+            //}
+        }
+        private async void ConnectToMyDevice()
+        {
             var ble = CrossBluetoothLE.Current;
             var state = ble.State;
             var adapter = CrossBluetoothLE.Current.Adapter;
+            _cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(5));
 
-            //var pairedDevices = adapter.GetSystemConnectedOrPairedDevices();
-            adapter.StartScanningForDevicesAsync();
-            var pairedDevices = adapter.ConnectedDevices;
-            if (pairedDevices.Count > 0)
+            //await adapter.StopScanningForDevicesAsync();
+            using (device = await adapter.ConnectToKnownDeviceAsync(
+                new Guid("00001101-0000-1000-8000-00805F9B34FB"), new ConnectParameters(true, false),
+                cancellationToken: _cancellationTokenSource.Token))
             {
-                foreach (var device in pairedDevices)
+
+                if (device.State == DeviceState.Connected)
                 {
-                    //pairedDevicesArrayAdapter.Add(device.Name + "\n" + device.Address);
+                    //Debug.WriteLine("OK");
                 }
+                else
+                {
+                   // Debug.WriteLine("Error");
+                }
+
             }
         }
+
     }
 }
